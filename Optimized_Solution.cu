b88b98b3fe225094d452560f31063026f8873421
PiroJOJO <sonymitchel5@gmail.com>
2023-04-25 18:51:40 +0000

    Created with graph

--- Optimized_Solution.cu
+++ Optimized_Solution.cu
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <cuda_runtime.h>
 #include <cub/cub.cuh>
 //Функция, которая высчитывает разницу между двумя массивами 
 __global__ void my_sub(double* arr, double* new_arr, double* c, int n)
@@ -10,7 +11,7 @@ __global__ void my_sub(double* arr, double* new_arr, double* c, int n)
     unsigned int j = blockIdx.y * blockDim.y + threadIdx.y;//Высчитывается из логики - Номер блока на размер блока(кол-во поток) плюс номер потока
     if((i > 0 && i < n-1) && (j > 0 && j < n-1))
     {
-        c[i*n + j] = new_arr[i*n + j] - arr[i*n + j];//Обращение по индексу по логике преобразования матрицы в одномерный массив
+        c[i*n + j] = fabs(new_arr[i*n + j] - arr[i*n + j]);//Обращение по индексу по логике преобразования матрицы в одномерный массив
     }
 } 
 //Функция, которая высчитвает средние значения для обновления сетки
@@ -23,7 +24,7 @@ __global__ void update(double* arr, double* new_arr, int n)
         new_arr[i*n + j] = 0.25 * (arr[i*n + j - 1] + arr[i*n + j + 1] + arr[(i - 1)*n + j] + arr[(i + 1)*n + j]);
     }
 } 
-__constant__ double step;
+
 __global__ void fill(double* arr, double* new_arr, int n)
 {
     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -34,12 +35,18 @@ __global__ void fill(double* arr, double* new_arr, int n)
     arr[n * (n - 1)] = new_arr[n * (n - 1)] = 20;
     if(i > 0 && i < n-1)
     {
-        arr[i] = new_arr[i] = arr[i - 1] + step;
-        arr[n*(n-1) + i] = new_arr[n*(n-1) + i]= arr[n*(n-1) + i - 1] + step;
-        arr[n*i]= new_arr[n*i] = arr[n*(i-1)] + step;
-        arr[n*i + n - 1] = new_arr[n*i + n - 1]= arr[n*(i-1) + n - 1] + step;
+        arr[i] = new_arr[i] = arr[0] + (10.0 / (n-1)) * i;
+        arr[n*(n-1) + i] = new_arr[n*(n-1) + i] = arr[n - 1] + 10.0 / (n-1) * i;
+        arr[n*i]= new_arr[n*i] = arr[0] + 10.0 / (n-1) * i;
+        arr[n*i + n - 1] = new_arr[n*i + n - 1] = arr[n-1] + 10.0 / (n-1) * i;
     }
 } 
+void swap(double* &a, double* &b)
+{
+    double *temp = a;
+    a = b;
+    b = temp;
+}
 //Функция для просмотра матрицы
 void print_matrix(double* vec, size_t n)
 {
@@ -47,7 +54,7 @@ void print_matrix(double* vec, size_t n)
     {
         for (size_t j = 0; j < n; ++j)
         {
-            std::cout<<vec[n*i +j]<<' ';
+            std::cout<<vec[n*i + j]<<' ';
         }
         std::cout<<std::endl;
     }
@@ -65,44 +72,36 @@ int main(int argc, char *argv[]) {
     double error = std::stod(argv[2]);//Значение ошибки
     size_t iter = std::stoi(argv[4]);//Количество итераций
     size_t n = std::stoi(argv[6]);//Размер сетки 
+    //cudaSetDevice(0);
 
     //Объявляем необходимы перменные 
-    double* vec;
-    double* new_vec;//Массив для значений на текущем шаге
-    double* tmp;
-    cudaMalloc((void **)&vec, sizeof(double)*n*n);
-    cudaMalloc((void **)&new_vec, sizeof(double)*n*n);
-    cudaMalloc((void **)&tmp, sizeof(double)*n*n);
+    double* vec = new double[n*n];//Массив для значений на предыдущем шаге
+    double* new_vec = new double[n*n];//Массив для значений на текущем шаге
+    double* tmp = new double[n*n];//Вспомогаетльный массив для сохранения результата для следующей итерации 
+    double* vec_d;
+    double* new_vec_d;//Массив для значений на текущем шаге
+    double* tmp_d;
+    cudaMalloc((void **)&vec_d, sizeof(double)*n*n);
+    cudaMalloc((void **)&new_vec_d, sizeof(double)*n*n);
+    cudaMalloc((void **)&tmp_d, sizeof(double)*n*n);
+    cudaMemcpy(vec_d, vec, sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(new_vec_d, new_vec, sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(tmp_d, tmp, sizeof(double), cudaMemcpyHostToDevice);
+
     double max_error = error + 1; //Объявление максимальной ошибки 
     size_t it = 0;//Счетчик итераций
-    double stepx = 10/(n-1);
-    cudaMemcpyToSymbol(step, &stepx, sizeof(double));
+    // double stepx = 10/(n-1);
+    // cudaMemcpyToSymbol(step, &stepx, sizeof(double));
 
     //Задаем размер блока и сетки 
     dim3 BLOCK_SIZE = dim3(32, 32);//Размер блока - количество потоков
     dim3 GRID_SIZE = dim3(ceil(n/32.), ceil(n/32.));//Размер сетки - количество блоков
     //Заполнение угловых значений
     //Не забываем, что мы матрицу представляем, как одномерный вектор(вытягиваем ее по по строкам)
-
-    print_matrix(vec, n);
-
-// #pragma acc data copy(new_vec[0:n*n], vec[0:n*n]) //Переносим занчения на видеокарту
-//     {
-//         //Заполнение рамок матриц
-// #pragma acc parallel loop independent//Создание ядра для распарреллеливания цикла
-//         for (size_t i = 1; i < n - 1; ++i) 
-//         {
-//             vec[i] = steps(n, i, vec[0], vec[n]); //Заполнение значениями для первой строки матрицы
-//             vec[n * i] = steps(n, i, vec[n], vec[n * n]);//Заполнение значениями для первого столбца матрицы
-//             vec[(n - 1) * n + i] = steps(n, i, vec[n * (n - 1) + 1], vec[n * n]);//Заполнение значениями послендней строки матрицы
-//             vec[i * n + n - 1] = steps(n, i, vec[0], vec[n * (n - 1) + 1]);//Заполнение значениями последнего столбца матрицы
-//         }
-//     }
-
-
-
-
-
+    fill<<<GRID_SIZE, BLOCK_SIZE>>>(vec_d, new_vec_d, n);
+    cudaMemcpy(vec, vec_d, sizeof(double)*n*n, cudaMemcpyDeviceToHost);
+    cudaMemcpy(new_vec, new_vec_d, sizeof(double)*n*n, cudaMemcpyDeviceToHost);
+ 
     //Также инициализируем переменную для расчета максимальной ошибки на cuda
     double* max_errorx;
     cudaMalloc(&max_errorx, sizeof(double));
@@ -114,39 +113,40 @@ int main(int argc, char *argv[]) {
     cub::DeviceReduce::Max(store, bytes, vec, max_errorx, n*n);
     // Allocate temporary storage
 	cudaMalloc(&store, bytes);
-    //Цикл основного алгоритма 
-        while(error < max_error && it < iter)
-	    {        
-            it++;
-            update<<<GRID_SIZE,BLOCK_SIZE>>>(vec, new_vec, n);
+    //Цикл основного алгоритма
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+    cudaGraph_t graph;
+    cudaGraphExec_t instance;
 
-        if (it % n == 0)
-        {
-            my_sub<<<GRID_SIZE, BLOCK_SIZE>>>(vec, new_vec, tmp, n);
-	        cub::DeviceReduce::Max(store, bytes, tmp, max_errorx, n*n);
-            // Allocate temporary storage
-	        cudaMalloc(&store, bytes);
-            // Run max-reduction
-	        cub::DeviceReduce::Max(store, bytes, tmp, max_errorx, n*n);
-            cudaMemcpy(&max_error, max_errorx, sizeof(double), cudaMemcpyDeviceToHost);//Обновление ошибки на CPU
-        }
+    cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+    for(int i = 0; i<n*n/4;i+=2)
+    {
+        update<<<GRID_SIZE,BLOCK_SIZE, 0, stream>>>(new_vec_d, vec_d, n);
+        update<<<GRID_SIZE,BLOCK_SIZE, 0, stream>>>(vec_d, new_vec_d, n);
+    }
+    cudaStreamEndCapture(stream, &graph);
+    cudaGraphInstantiate(&instance, graph, NULL, NULL, 0);
 
-         //Обмен между массивами с старыми значенями и с новыми через указатель
-        // std::swap(vec, new_vec);
+    while(error < max_error && it < iter)
+	{        
+        it+=n*n/2;
+        cudaGraphLaunch(instance, stream);
+        my_sub<<<GRID_SIZE, BLOCK_SIZE, 0, stream>>>(vec_d, new_vec_d, tmp_d, n);
+	    cub::DeviceReduce::Max(store, bytes, tmp_d, max_errorx, n*n);
+        cudaMemcpy(&max_error, max_errorx, sizeof(double), cudaMemcpyDeviceToHost);//Обновление ошибки на CPU
 
-        // acc_attach((void**)&vec);
-        // acc_attach((void**)&new_vec);
-
-        }
-    
+    }
 
     std::cout<<"Error: "<<max_error<<std::endl;
     auto end = std::chrono::steady_clock::now();
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end-begin);
+    // cudaDeviceSynchronize();
+    // cudaMemcpy(vec, vec_d, sizeof(double)*n*n, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(new_vec, new_vec_d, sizeof(double)*n*n, cudaMemcpyDeviceToHost);
+    // print_matrix(vec, n);
     std::cout<<"time: "<<elapsed_ms.count()<<" mcs\n";
     std::cout<<"Iterations: "<<it<<std::endl;
-
-    //print_matrix(vec, n);
     delete [] vec; 
     delete [] new_vec;
     cudaFree(vec);
